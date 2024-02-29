b961d83f8fda5fb26e8bf3491c3922f5143d5b4a
thiago <thiagosilva327@live.com>
2024-02-28 23:29:48 -0300

    trash: deleta funcoes no controller

--- triagem-saude/app/Http/Controllers/PacienteController.php
+++ triagem-saude/app/Http/Controllers/PacienteController.php
@@ -16,14 +16,6 @@ class PacienteController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
@@ -35,14 +27,6 @@ class PacienteController extends Controller
      * Display the specified resource.
      */
     public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
     {
         //
     }
