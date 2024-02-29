70eb266c456ccbd4a9c73df7ba76d23129559a3c
thiago <thiagosilva327@live.com>
2024-02-28 23:44:29 -0300

    fix: altera nome do recurso

--- triagem-saude/routes/api.php
+++ triagem-saude/routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PacienteController;
 
-Route::apiResource('triagem-saude', PacienteController::class);
+Route::apiResource('triagem', PacienteController::class);
